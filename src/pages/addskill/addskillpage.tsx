39c239f33e5711913c56d6c5234d031a1c8d0f90
Mbarak Mbigo <mbarak.mbigo@andela.com>
2017-07-17 18:45:08 +0300

    fix(merge conflict)

---
+++ src/pages/addskill/addskillpage.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import {Button} from 'react-toolbox/lib/button';
+
+class SubmitButton extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
+  }
+  public render() {
+    return (
+      <Button label="Add skill" accent />
+    );
+  }
+}
+
+export default SubmitButton;
---
+++ src/pages/addskill/okbbs.component.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+class Okbbs extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
+  }
+  public render() {
+    return(
+      <div>
+        OKBBs Accordion
+      </div>
+    );
+  }
+}
+
+export default Okbbs;
+
