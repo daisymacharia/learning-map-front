92ff4430b56d9da9dfd355c5989b67888e5e15b5
Mbarak Mbigo <mbarak.mbigo@andela.com>
2017-08-01 18:28:48 +0300

    feat(contributions-card): refactor contributions list - refactor cards interface [Delivers #149667055]

--- src/pages/contributions/ContributionList.component.tsx
+++ src/pages/contributions/ContributionList.component.tsx
@@ -1,20 +1,21 @@
 import * as React from 'react';
 import ContributionsCard from './ContributionsCard';
 
-const ContributionList = ({contributions}) => {
-    if (contributions.contributions) {
-      return (
-        <div className="container">
-          {
-            contributions.contributions.map((contribution) => {
-              return <ContributionsCard key={contribution.contribution_id}
-                  contributions={contribution}/>;
-            })
-          }
-        </div>
-      );
-    }
-    return <div></div>;
+const ContributionList = ({ contributions }) => {
+  if (contributions) {
+    return (
+      <div className="container">
+        {
+          contributions.map((contribution) => {
+            return <ContributionsCard key={contribution.contribution_id}
+                                      contributions={contribution} />;
+          })
+        }
+      </div>
+    );
+  }
+
+  return <div></div>;
 };
 
 export default ContributionList;
